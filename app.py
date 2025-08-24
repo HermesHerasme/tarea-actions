5c162cb6a0e975b4753c39f0136ce35becfb192b
Hermes_Xcode <157546328+HermesHerasme@users.noreply.github.com>
2025-08-24 18:19:14 +0000

    tu mensaje de commit

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def home():
     return "🚀 Hola desde Flask en Docker y Render!"
 
 if __name__ == "__main__":
-    # Render usa la variable $PORT, si no existe, usa 8000 localmente
+    # Render usa la variable $PORT, si no existe, usaa 8000 localmente
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
